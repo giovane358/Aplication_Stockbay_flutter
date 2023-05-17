61cd1b33f48747394d690c6cce5d0cf90c7c1586
giovane358 <givanesilva.etec@gmail.com>
2023-05-17 19:29:32 -0300

    Upgrade

--- lib/screen/walcaomescreen/body.dart
+++ lib/screen/walcaomescreen/body.dart
@@ -14,81 +14,78 @@ class Body extends StatelessWidget {
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     return Backgroud(
-      child: SafeArea(
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Image.asset(
-                'images/img/walcome.png',
-                width: size.width * 0.5,
-              ),
-              const Text(
-                textAlign: TextAlign.center,
-                'Gerenciar seu negócio rapidamente com o Stockbay',
-                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
-              ),
-              SizedBox(
-                height: size.height * 0.02,
-              ),
-              const Text(
-                textAlign: TextAlign.center,
-                'Grandes descontos e ofertas quando você compra.',
-                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 12),
-              ),
-              ElevatedButtonContaner(
-                child: GestureDetector(
-                  onTap: () => Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => const LoginScreen(),
-                    ),
-                  ),
-                  child: const Text(
-                    textAlign: TextAlign.center,
-                    'Login',
-                    style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        color: Colors.white,
-                        fontSize: 18),
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              'images/img/walcome.png',
+              width: size.width * 0.5,
+            ),
+            const Text(
+              textAlign: TextAlign.center,
+              'Gerenciar seu negócio rapidamente com o Stockbay',
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+            ),
+            SizedBox(
+              height: size.height * 0.02,
+            ),
+            const Text(
+              textAlign: TextAlign.center,
+              'Grandes descontos e ofertas quando você compra.',
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 12),
+            ),
+            ElevatedButtonContaner(
+              child: GestureDetector(
+                onTap: () => Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const LoginScreen(),
                   ),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 25),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: const [
-                    Expanded(
-                      child: Divider(
-                        thickness: 0.5,
-                        color: Colors.black,
-                      ),
-                    ),
-                  ],
+                child: const Text(
+                  textAlign: TextAlign.center,
+                  'Login',
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                      fontSize: 18),
                 ),
               ),
-              ElevatedButtonContaner(
-                child: GestureDetector(
-                  onTap: () => Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => const RegisterScreen(),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: const [
+                  Expanded(
+                    child: Divider(
+                      thickness: 0.5,
+                      color: Colors.black,
                     ),
                   ),
-                  child: const Text(
-                    textAlign: TextAlign.center,
-                    'Registre-se',
-                    style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        color: Colors.white,
-                        fontSize: 18),
+                ],
+              ),
+            ),
+            ElevatedButtonContaner(
+              child: GestureDetector(
+                onTap: () => Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const RegisterScreen(),
                   ),
                 ),
-              )
-            ],
-          ),
+                child: const Text(
+                  textAlign: TextAlign.center,
+                  'Registre-se',
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                      fontSize: 18),
+                ),
+              ),
+            )
+          ],
         ),
       ),
     );
